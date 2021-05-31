92be87e567562adb37f48cdbf91baac18c38e354
Farhaan Beeharry <farhaanbeeharry.ms@gmail.com>
2021-05-31 04:26:23 +0400

    fixed space bar bug when typing

--- web/lib/main.dart
+++ web/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:web/Widgets/ForgotPassword/ForgotPasswordView.dart';
 import 'package:web/Widgets/Homepage/Homepage.dart';
 import 'package:web/Widgets/NewPassword/NewPasswordView.dart';
@@ -7,8 +8,11 @@ import 'package:web/Widgets/Signup/SignupView.dart';
 import 'Widgets/Login/LoginView.dart';
 
 void main() {
+  final shortcuts = WidgetsApp.defaultShortcuts;
+  shortcuts[LogicalKeySet(LogicalKeyboardKey.space)] = ActivateIntent();
   runApp(
     new MaterialApp(
+      shortcuts: shortcuts,
       initialRoute: '/Login',
       routes: {
         '/Login': (context) => LoginPage(),
